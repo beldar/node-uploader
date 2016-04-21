55046127571894ea9beab68b232bd75954f87bfd
beldar <beldar.cat@gmail.com>
2016-04-21 18:10:23 +0100

    Fix progress

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -7,6 +7,7 @@ const App = {
     this.dropzone = document.getElementById('dropzone');
     this.getname = document.getElementById('getname');
     this.wrapper = document.getElementById('wrapper');
+    this.progressBar = document.getElementById('progress');
 
     if (localStorage.getItem('username')) {
       this.username = localStorage.getItem('username');
@@ -53,72 +54,86 @@ const App = {
     });
   },
 
-  setDropzone() {
+  resetState() {
     this.state = {
-      totalFiles: 0,
-      completedFiles: 0,
-      totalSize: 0,
-      completedSize: 0,
+      files: {},
       timeElapsed: 0
     };
 
-    this.progressBar = document.getElementById('progress');
+    this.progressBar.style.width = 0;
+  },
 
+  setDropzone() {
     this.wrapper.addEventListener('dragenter', () => this.wrapper.classList.add('enter'));
-    this.wrapper.addEventListener('dragleave', () => this.wrapper.classList.remove('enter'));
-    this.wrapper.addEventListener('dragover', (e) => {
-      e.preventDefault();
-      this.wrapper.classList.remove('enter');
-      return false;
-    });
-
+    this.wrapper.addEventListener('dragover', (e) => e.preventDefault());
     this.wrapper.addEventListener('drop', (e) => {
       e.preventDefault();
 
+      this.wrapper.classList.remove('enter');
+
       let files = e.target.files || e.dataTransfer.files;
 
-      this.files = files;
+      console.log('Drop', files);
 
-      this.uploadFiles();
+      this.resetState();
+
+      this.uploadFiles(files);
 
       return false;
-
     });
   },
 
-  uploadFiles() {
-    let files = this.files;
-
+  uploadFiles(files) {
     for (var i = 0; i < files.length; i++) {
-        file = files[i];
-        this.state.totalFiles++;
-        this.state.totalSize += file.size;
-
-        this.uploadFile(file);
-        console.log(file);
+      let file = files[i];
+      this.uploadFile(file.name, file.size, file.path);
     }
   },
 
-  uploadFile(file) {
+  uploadFile(name, size, path) {
+    let file = {
+      name     : name,
+      size     : size,
+      path     : path,
+      written  : 0,
+      completed: false,
+      failed   : false
+    };
+
     if (! this.username) return;
 
+    if (file.name.indexOf('.DS_Store') !== -1 || file.name.indexOf('Thumbs.db') !== -1) return;
+
+    console.log(file);
     if (fs.lstatSync(file.path).isDirectory()) {
       this.uploadDir(file);
       return;
     }
 
-    let path = this.username.trim().replace(' ', '-') + '/' + file.name.trim().replace(' ', '-');
+    this.state.files[file.name] = file;
 
-    console.log('Upload to: ' + path);
+    let awsPath = this.username.trim().replace(' ', '-') + '/' + file.name.trim().replace(' ', '-');
+    let req = this.client.putFile(file.path, awsPath, (err, res) => {
+      if (err) {
+        file.failed = true;
+        console.log(this.state.files);
+        return;
+      }
 
-    let req = this.client.putFile(file.path, path, this.fileDone);
+      file.completed = true;
 
-    req.on('progress', this.updateProgress);
-    req.on('response', this.fileCompleted);
-    req.on('error', this.fileFailed)
+      console.log(this.state.files);
+    });
+
+    req.on('progress', (e) => {
+      file.written = e.written;
+      this.updateProgress();
+    });
   },
 
   uploadDir(file) {
+    console.log('uploadDir', file);
+
     dir.files(file.path, (err, files) => {
         if (err) throw err;
         console.log(files);
@@ -127,38 +142,40 @@ const App = {
           let stat = fs.lstatSync(path);
           let name = this.getName(path);
 
-          this.state.totalFiles++;
-          this.state.totalSize += stat.size;
-
-          let file = {
-            name: name,
-            path: path
-          };
-
-          this.uploadFile(file);
+          this.uploadFile(name, stat.size, path);
         });
     });
   },
 
-  updateProgress(e) {
-    console.log('Progress', e);
-    App.state.completedSize += e.written;
+  updateProgress() {
+    console.log(this.state.files);
 
-    let percent = ((App.state.completedSize / App.state.totalSize) * 100).toFixed(2);
+    let keys = Object.keys(this.state.files);
 
-    App.progressBar.style.width = percent + '%';
-  },
+    let total = keys.reduce((previous, key) => {
+                  return previous + this.state.files[key].size;
+                }, 0);
 
-  fileCompleted(e) {
-    console.log('File completed', e);
+    let completed = keys.reduce((previous, key) => {
+                      return previous + this.state.files[key].written;
+                    }, 0);
+
+    let percent = ((completed / total) * 100).toFixed(2);
+
+    console.log('Progress', percent, this.state);
+
+    this.progressBar.style.width = percent + '%';
   },
 
   fileDone(err, res) {
     console.log('File done', err, res);
-  },
 
-  fileFailed(e) {
-    console.error('File failed', e);
+    if (err) {
+      this.state.filesFailed.push(err);
+      return;
+    }
+
+    this.state.filesUploaded.push(res.req.path);
   },
 
   getName(path) {
